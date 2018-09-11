dfd9210f38d9d54247b60154a56320f1baf92275
Moaad <Moaad02@gmail.com>
2018-09-11 23:46:36 +0300

    BinaryTree First Drop With add() And find() abilities

---
+++ src/main/java/trees/BinaryTree.java
@@ -0,0 +1,66 @@
+package trees;
+
+public class BinaryTree {
+    private BinaryTreeNode root;
+
+    private int size;
+
+    public BinaryTree() {
+    }
+
+    public void add(final int value) {
+        if (root == null) {
+            root = new BinaryTreeNode(value);
+            return;
+        }
+        add(root, value);
+    }
+
+    private BinaryTreeNode add(final BinaryTreeNode current, final int value) {
+        if (current == null) {
+            size++;
+            return new BinaryTreeNode(value);
+        }
+        if (value > current.getValue()) {
+            current.setRightNode(add(current.getRightNode(), value));
+        } else if (value < current.getValue()) {
+            current.setLeftNode(add(current.getLeftNode(), value));
+        }
+        size++;
+        return current;
+    }
+
+
+    public BinaryTreeNode find(final int value) {
+
+        return find(root, value);
+
+    }
+
+    private BinaryTreeNode find(final BinaryTreeNode current, final int value) {
+
+        if (current == null) return null;
+        if (current.getValue() == value) return current;
+
+        if (value > current.getValue())
+            return find(current.getRightNode(), value);
+        else return find(current.getLeftNode(), value);
+
+    }
+/*
+    public boolean remove(int value) {
+        BinaryTreeNode foundNode = find(value);
+        if (foundNode == null) throw new IllegalArgumentException("value : " + value + " Does not exist");
+        if (foundNode.getLeftNode() == null && foundNode.getRightNode() == null)
+            return false;
+    }*/
+
+
+    public BinaryTreeNode getRoot() {
+        return root;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
---
+++ src/test/java/trees/BinaryTreeNodeTests.java
@@ -0,0 +1,135 @@
+package trees;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class BinaryTreeNodeTests {
+
+    BinaryTree binaryTree;
+    List<Integer> data = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    @BeforeMethod
+    public void init() {
+        binaryTree = new BinaryTree();
+
+    }
+
+    @Test
+    public void whenAddingFirstElementShouldBeAParentNode() {
+
+        //When
+        binaryTree.add(data.get(0));
+
+        //Then
+        Assert.assertEquals(binaryTree.getRoot().getValue(), data.get(0).intValue());
+    }
+
+
+    @Test
+    public void biggerValueShouldGoToTheRight() {
+
+        //When
+        binaryTree.add(data.get(1));
+        binaryTree.add(data.get(2));
+
+
+        //Then
+        Assert.assertEquals(binaryTree.getRoot().getRightNode().getValue(), data.get(2).intValue());
+
+    }
+
+    @Test
+    public void smallerValueShouldGoToTheLeft() {
+
+        //When
+        binaryTree.add(data.get(1));
+        binaryTree.add(data.get(0));
+
+
+        //Then
+        Assert.assertEquals(binaryTree.getRoot().getLeftNode().getValue(), data.get(0).intValue());
+
+    }
+
+    @Test
+    public void whenAddingValueSizeShouldIncreaseByOne() {
+
+        //When
+        binaryTree.add(data.get(1));
+        binaryTree.add(data.get(0));
+
+
+        //Then
+        Assert.assertEquals(binaryTree.getSize(), 2);
+    }
+
+    @Test
+    public void searchingForNonExistingValueShouldReturnNull() {
+        //When
+        BinaryTreeNode node = binaryTree.find(3);
+
+        //Then
+        Assert.assertEquals(null, node);
+
+    }
+
+
+    @Test
+    public void searchingForExistingValueShouldReturnIt() {
+
+        //Given
+        binaryTree.add(data.get(1));
+        binaryTree.add(data.get(2));
+        binaryTree.add(data.get(0));
+
+
+        //When
+        BinaryTreeNode node = binaryTree.find(data.get(1));
+
+        //Then
+        var rootNode = new BinaryTreeNode(data.get(1));
+        rootNode.setLeftNode(new BinaryTreeNode(data.get(0)));
+        rootNode.setRightNode(new BinaryTreeNode(data.get(2)));
+
+        Assert.assertEquals(node.getValue(), rootNode.getValue());
+        Assert.assertEquals(node.getRightNode().getValue(), rootNode.getRightNode().getValue());
+        Assert.assertEquals(node.getLeftNode().getValue(), rootNode.getLeftNode().getValue());
+
+
+        //When
+        node = binaryTree.find(data.get(2));
+
+        //Then
+        Assert.assertEquals(node.getValue(), data.get(2).intValue());
+        Assert.assertEquals(node.getRightNode(), null);
+        Assert.assertEquals(node.getLeftNode(), null);
+
+        //When
+        node = binaryTree.find(data.get(0));
+
+        //Then
+        Assert.assertEquals(node.getValue(), data.get(0).intValue());
+        Assert.assertEquals(node.getRightNode(), null);
+        Assert.assertEquals(node.getLeftNode(), null);
+
+    }
+/*
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void removingValueThatDoesNotExistWillThrowException() {
+        //Given
+        binaryTree.add(data.get(4));
+        binaryTree.add(data.get(3));
+        binaryTree.add(data.get(2));
+        binaryTree.add(data.get(1));
+        binaryTree.add(data.get(0));
+        binaryTree.add(data.get(5));
+
+        //When
+        binaryTree.remove(data.get(6));
+    }*/
+
+}
