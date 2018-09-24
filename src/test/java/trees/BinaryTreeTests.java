395a0da8abc2dcc219e6ff15cffc89b6cd5b2782
moaada <moaada@radware.com>
2018-09-24 10:20:30 +0300

    Add remove and traverse support

--- src/test/java/trees/BinaryTreeTests.java
+++ src/test/java/trees/BinaryTreeTests.java
@@ -1,15 +1,15 @@
 package trees;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static org.testng.Assert.*;
+
 public class BinaryTreeTests {
 
-    BinaryTree binaryTree;
-    List<Integer> data = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    BinaryTree<Integer> binaryTree;
 
     @BeforeMethod
     public void init() {
@@ -21,10 +21,10 @@ public class BinaryTreeTests {
     public void whenAddingFirstElementShouldBeAParentNode() {
 
         //When
-        binaryTree.add(data.get(0));
+        binaryTree.add(1);
 
         //Then
-        Assert.assertEquals(binaryTree.getRoot().getValue(), data.get(0).intValue());
+        assertEquals(binaryTree.getRoot().getValue(), 1);
     }
 
 
@@ -32,12 +32,12 @@ public class BinaryTreeTests {
     public void biggerValueShouldGoToTheRight() {
 
         //When
-        binaryTree.add(data.get(1));
-        binaryTree.add(data.get(2));
+        binaryTree.add(1);
+        binaryTree.add(2);
 
 
         //Then
-        Assert.assertEquals(binaryTree.getRoot().getRightNode().getValue(), data.get(2).intValue());
+        assertEquals(binaryTree.getRoot().getRightNode().getValue(), 2);
 
     }
 
@@ -45,12 +45,12 @@ public class BinaryTreeTests {
     public void smallerValueShouldGoToTheLeft() {
 
         //When
-        binaryTree.add(data.get(1));
-        binaryTree.add(data.get(0));
+        binaryTree.add(1);
+        binaryTree.add(0);
 
 
         //Then
-        Assert.assertEquals(binaryTree.getRoot().getLeftNode().getValue(), data.get(0).intValue());
+        assertEquals(binaryTree.getRoot().getLeftNode().getValue(), 0);
 
     }
 
@@ -58,88 +58,203 @@ public class BinaryTreeTests {
     public void whenAddingValueSizeShouldIncreaseByOne() {
 
         //When
-        binaryTree.add(data.get(2));
+        binaryTree.add(2);
 
         //Then
-        Assert.assertEquals(binaryTree.getSize(), 1);
+        assertEquals(binaryTree.getSize(), 1);
 
         //When
-        binaryTree.add(data.get(1));
+        binaryTree.add(1);
 
         //Then
-        Assert.assertEquals(binaryTree.getSize(), 2);
+        assertEquals(binaryTree.getSize(), 2);
 
         //When
-        binaryTree.add(data.get(0));
+        binaryTree.add(0);
 
         //Then
-        Assert.assertEquals(binaryTree.getSize(), 3);
+        assertEquals(binaryTree.getSize(), 3);
     }
 
     @Test
-    public void searchingForNonExistingValueShouldReturnNull() {
-        //When
-        BinaryTreeNode node = binaryTree.find(3);
+    public void contains() {
 
-        //Then
-        Assert.assertEquals(null, node);
+        assertFalse(binaryTree.contains(0));
+
+        binaryTree.add(2);
+        binaryTree.add(1);
+        binaryTree.add(3);
+
+        assertTrue(binaryTree.contains(1));
+        assertTrue(binaryTree.contains(2));
+        assertTrue(binaryTree.contains(3));
+
 
     }
 
 
     @Test
-    public void searchingForExistingValueShouldReturnIt() {
-
+    public void removeValueThatDoesNotExistWillReturnFalse() {
         //Given
-        binaryTree.add(data.get(1));
-        binaryTree.add(data.get(2));
-        binaryTree.add(data.get(0));
+        binaryTree.add(5);
+        binaryTree.add(3);
+        binaryTree.add(2);
+        binaryTree.add(6);
+        binaryTree.add(4);
 
 
         //When
-        BinaryTreeNode node = binaryTree.find(data.get(1));
+        boolean status = binaryTree.remove(7);
 
         //Then
-        var rootNode = new BinaryTreeNode(data.get(1));
-        rootNode.setLeftNode(new BinaryTreeNode(data.get(0)));
-        rootNode.setRightNode(new BinaryTreeNode(data.get(2)));
+        assertFalse(status);
+    }
 
-        Assert.assertEquals(node.getValue(), rootNode.getValue());
-        Assert.assertEquals(node.getRightNode().getValue(), rootNode.getRightNode().getValue());
-        Assert.assertEquals(node.getLeftNode().getValue(), rootNode.getLeftNode().getValue());
+    @Test
+    public void removeNodeWhichIsALeafShouldUpdateParentNode() {
+        //Given
+        binaryTree.add(1);
+        binaryTree.add(0);
+        binaryTree.add(2);
 
-
-        //When
-        node = binaryTree.find(data.get(2));
-
-        //Then
-        Assert.assertEquals(node.getValue(), data.get(2).intValue());
-        Assert.assertEquals(node.getRightNode(), null);
-        Assert.assertEquals(node.getLeftNode(), null);
-
-        //When
-        node = binaryTree.find(data.get(0));
+        //When & Then
+        assertEquals(binaryTree.remove(0), true);
+        assertEquals(binaryTree.remove(2), true);
 
         //Then
-        Assert.assertEquals(node.getValue(), data.get(0).intValue());
-        Assert.assertEquals(node.getRightNode(), null);
-        Assert.assertEquals(node.getLeftNode(), null);
+        assertEquals(binaryTree.getRoot().getLeftNode(), null);
+        assertEquals(binaryTree.getRoot().getRightNode(), null);
+        assertEquals(binaryTree.getRoot().getValue(), 1);
+        assertEquals(binaryTree.getSize(), 1);
+
 
     }
-/*
 
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void removingValueThatDoesNotExistWillThrowException() {
+    @Test
+    public void removeNodeWhichHasOneChildOnlyShouldReplaceIt() {
+
         //Given
-        binaryTree.add(data.get(4));
-        binaryTree.add(data.get(3));
-        binaryTree.add(data.get(2));
-        binaryTree.add(data.get(1));
-        binaryTree.add(data.get(0));
-        binaryTree.add(data.get(5));
+        binaryTree.add(6);
+        binaryTree.add(5);
+        binaryTree.add(3);
+        binaryTree.add(7);
+        binaryTree.add(8);
+
+        //When & Then
+        assertEquals(binaryTree.remove(5), true);
+        assertEquals(binaryTree.remove(7), true);
+
+        //Then
+        assertEquals(binaryTree.getRoot().getValue(), 6);
+        assertEquals(binaryTree.getRoot().getLeftNode().getValue(), 3);
+        assertEquals(binaryTree.getRoot().getRightNode().getValue(), 8);
+        assertFalse(binaryTree.contains(5));
+        assertFalse(binaryTree.contains(7));
+    }
+
+    @Test
+    public void removeNodeWhichHasTwoChildsShouldBeReplacedWithItsRightLeftMostChild() {
+        //Given
+        binaryTree.add(6);
+        binaryTree.add(8);
+        binaryTree.add(7);
+        binaryTree.add(9);
+
+        binaryTree.add(4);
+        binaryTree.add(5);
+        binaryTree.add(3);
+
+        //When & Then
+        assertEquals(binaryTree.remove(4), true);
+        assertEquals(binaryTree.remove(8), true);
+
+        //Then
+        assertEquals(binaryTree.getRoot().getValue(), 6);
+        assertEquals(binaryTree.getRoot().getLeftNode().getValue(), 5);
+        assertEquals(binaryTree.getRoot().getRightNode().getValue(), 9);
+        assertFalse(binaryTree.contains(4));
+        assertFalse(binaryTree.contains(8));
+    }
+
+    @Test
+    public void removeValueDecreaseSize() {
+        //Given
+        binaryTree.add(6);
+        binaryTree.add(8);
+        binaryTree.add(7);
+        binaryTree.add(9);
+
+
+        //When & Then
+        assertEquals(binaryTree.remove(7), true);
+        assertEquals(binaryTree.remove(9), true);
+
+        //Then
+        assertEquals(binaryTree.getSize(), 2);
+
+    }
+
+
+    @Test
+    public void preOrderTraversal() {
+        //Given
+        binaryTree.add(5);
+
+        binaryTree.add(3);
+        binaryTree.add(4);
+        binaryTree.add(2);
+
+        binaryTree.add(7);
+        binaryTree.add(6);
+        binaryTree.add(8);
 
         //When
-        binaryTree.remove(data.get(6));
-    }*/
+        List<Integer> actual = binaryTree.preOrderTraversal();
+        var expected = List.of(5, 3, 2, 4, 7, 6, 8);
 
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void inOrderTraversal() {
+        //Given
+        binaryTree.add(5);
+
+        binaryTree.add(3);
+        binaryTree.add(4);
+        binaryTree.add(2);
+
+        binaryTree.add(7);
+        binaryTree.add(6);
+        binaryTree.add(8);
+
+        //When
+        List<Integer> actual = binaryTree.inOrderTraversal();
+        var expected = List.of(2, 3, 4, 5, 6, 7, 8);
+
+        assertEquals(actual, expected);
+
+    }
+
+
+    @Test
+    public void postOrderTraversal() {
+        //Given
+        binaryTree.add(5);
+
+        binaryTree.add(3);
+        binaryTree.add(4);
+        binaryTree.add(2);
+
+        binaryTree.add(7);
+        binaryTree.add(6);
+        binaryTree.add(8);
+
+        //When
+        List<Integer> actual = binaryTree.postOrderTraversal();
+        var expected = List.of(2, 4, 3, 6, 8, 7, 5);
+
+        assertEquals(actual, expected);
+    }
 }
